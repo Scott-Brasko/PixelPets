93737b8d7b8be88b3f4eb88a98841a3f24aa6d0d
Scott Brasko <scottbrasko@gmail.com>
2023-04-19 19:14:42 -0500

    added to dog constructor

--- Dog.js
+++ Dog.js
@@ -1,21 +1,36 @@
 // dog constructor (name)
-  // call after user inputs
-  // name prop
-  // speed prop
-  // ownerName prop
-  // frame prop - keep track of current frame
-  // state prop - default idle 
-      // idle
-      // eat
-      // walk
-      // run
+class Dog {
+  constructor(body, name, owner) {
+    // call after user inputs
+    // name prop
+    this.name = name;
+    // speed prop
+    this.speed = 250;
+    // ownerName prop
+    this.owner = owner;
 
-// create img & append to HTML
-// recursively call monitor method (checks state)
-// use settime out
+    // frame prop - keep track of current frame
+    this.frame = 0;
 
-// monitor
-// idle
-// eat 
-// walk
-// run
+    // state prop - default idle
+    // idle
+    // eat
+    // walk
+    // run
+    this.state = 'idle1';
+
+    // create img & append to HTML
+    this.node = document.createElement('img');
+    this.node.setAttribute('id', 'dog');
+    this.node.setAttribute('src',`src/assets/${this.state}/${this.state}-${this.frame}`);
+    body.appendChild(this.node);
+    // recursively call monitor method (checks state)
+    // use settime out
+  }
+
+  // monitor
+  // idle
+  // eat
+  // walk
+  // run
+}
